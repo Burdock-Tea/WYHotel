79f0bf89db8e843b6bea6aebb8e6911316037134
CodingBiginner <kuy930201@gmail.com>
2023-02-09 12:28:41 +0900

    test JAVA

--- src/test/java/com/ictproject/wyhotel/TestJava.java
+++ src/test/java/com/ictproject/wyhotel/TestJava.java
@@ -1,8 +1,17 @@
 package com.ictproject.wyhotel;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class TestJava {
 	
 	public static void main(String[] args) {	
+		
+		Collection<Integer> collectionTest = new ArrayList<Integer>();
+		
+		collectionTest.add(3);
+		
+		System.out.println(collectionTest);
 		
 	}
 	
